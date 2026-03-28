9c8e416d9a7d403ee4405282326e3c3b5378673b
MorokaPrince <luckyrakgama@gmail.com>
2026-03-28 18:51:51 +0200

    chore: configure custom domain www.gamawela.com

--- ga-mawela/next-sitemap.config.js
+++ ga-mawela/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ga-mawela-platform.vercel.app',
+  siteUrl: 'https://www.gamawela.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
